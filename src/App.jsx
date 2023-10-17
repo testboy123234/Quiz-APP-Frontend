390015a117d1c5dfb922ad780f1029e6eafef9a2
testing doonly <106380029+DPLakmal@users.noreply.github.com>
2023-10-17 22:35:35 +0530

    Inital commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div class="min-h-screen flex items-center justify-center">
+        <div class="bg-blue-500 p-8 text-white columns-2">
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+          </ul>
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
