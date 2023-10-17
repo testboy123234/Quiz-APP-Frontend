ef85a18d60e521c4e316b126b7f357e3433c6df5
Your Name <your.email@example.com>
2023-10-17 23:15:06 +0530

    Create Quiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import React from "react";
+import Quiz from './Quiz';
 
 const App = () => {
   return (
     <>
-      <div class="min-h-screen flex items-center justify-center">
-        <div class="bg-blue-500 p-8 text-white columns-2">
-          <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-          </ul>
-          <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-          </ul>
-        </div>
-      </div>
+      <Quiz />
     </>
   );
 };
